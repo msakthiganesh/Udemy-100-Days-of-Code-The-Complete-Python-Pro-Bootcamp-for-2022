7a5b3e06bdfbfe9147a4cd11a03d1dc4af3fe83d
Sakthi Ganesh <sakthiganesh1997@gmail.com>
2022-03-10 22:29:06 +0530

    added day 1 project - band name generator

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,7 @@
+# Udemy - 100 Days of Code: The Python Pro Bootcamp for 2022
+# Day 1: Project - Band Name Generator
+
+print("Welcome to the Band Name Generator.") 
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f'Your band name could be {city_name + " " + pet_name}')
