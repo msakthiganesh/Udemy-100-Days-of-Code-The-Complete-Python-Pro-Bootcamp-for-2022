df783a4a680a1a1e1b0ac00b7ab8a397afbbd87f
Sakthi Ganesh <sakthiganesh1997@gmail.com>
2022-04-05 21:29:07 +0530

    updated

--- Day 8/project_caesar_cipher.py
+++ Day 8/project_caesar_cipher.py
@@ -3,11 +3,23 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
+import itertools
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text:str, shift:int):
-    pass
+    encrypted_text = ""
+    
+    cycle_alph = itertools.cycle(alphabet)
+    cycle_alphabets = []
+    for i, val in enumerate(cycle_alph):
+        cycle_alphabets.append(val)
+        if i > 1000: break
+    
+    for char in text:
+        encrypted_text += cycle_alphabets[alphabet.index(char) + shift]
+    
+    return encrypted_text
+
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -22,3 +34,10 @@ def encrypt(text:str, shift:int):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+if direction == "encode":
+    encrypted_text = encrypt(text, shift)
+    print(encrypted_text)
+
+
+
